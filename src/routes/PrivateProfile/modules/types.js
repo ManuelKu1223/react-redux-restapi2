5a69fba1cde90a47bf8d6e08239227a50fc58ddd
Andrey1130 <rubyking171@gmail.com>
2019-04-04 22:39:54 +0300

    add types

--- src/routes/PrivateProfile/modules/types.js
+++ src/routes/PrivateProfile/modules/types.js
@@ -2,3 +2,7 @@ export const GET_USER_INFO = 'GET_USER_INFO';
 export const ADD_USER_INFO = 'ADD_USER_INFO';
 export const REMOVE_USER_INFO = 'REMOVE_USER_INFO';
 export const UPDATE_USER_INFO = 'UPDATE_USER_INFO';
+export const GET_USER_DATA = 'GET_USER_DATA';
+export const UPDATE_EMAIL = 'UPDATE_EMAIL';
+export const USER_LOGOUT = 'USER_LOGOUT';
+
